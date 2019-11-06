bf00525e52d458779e063837c8af51b47a33689c
Akane <soranyuxe@gmail.com>
2019-11-06 09:35:03 +0100

    camera

--- ClassroomMadness/Assets/Script/deplacement.cs
+++ ClassroomMadness/Assets/Script/deplacement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class deplacement : MonoBehaviour
 {
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    public bool enter = true;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-              Debug.Log("ça touche");  
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,7 +21,7 @@ public class deplacement : MonoBehaviour
         if (Input.GetKey(KeyCode.Z))
         {
             transform.Translate(0.1f, 0, 0);
-            Debug.Log("ça touche");
+
         }
         if (Input.GetKey(KeyCode.Q))
         {
@@ -31,12 +35,14 @@ public class deplacement : MonoBehaviour
         {
             transform.Translate(0, 0, -0.1f);
         }
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        //float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, h, 0);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("entered");
         transform.Translate(0, 0, 0);
-        Debug.Log("ça touche");
     }
 }
