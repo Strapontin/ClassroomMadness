85e4fd79e44faab879d7a74877a3dc8a47f77d3b
Ngu <gchaducnguyen@gmail.com>
2019-12-24 15:24:42 +0100

    Correction du dernier jour ydays 2019(bug sur l'asymétrie normalement réglé)

--- ClassroomMadness/Assets/notreProg/Guillaume/AvatarVRProf/YeuxDuVR.cs
+++ ClassroomMadness/Assets/notreProg/Guillaume/AvatarVRProf/YeuxDuVR.cs
@@ -6,6 +6,8 @@ public class YeuxDuVR : MonoBehaviour
 {
     public GameObject cam;
     public float haut;
+    public float z;
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,7 @@ public class YeuxDuVR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y - haut, cam.transform.position.z);
+        transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y - haut, cam.transform.position.z - z) + cam.transform.forward * distance;
         transform.rotation = Quaternion.Euler(cam.transform.rotation.eulerAngles.x, cam.transform.rotation.eulerAngles.y, cam.transform.rotation.eulerAngles.z);
     }
 }
---
+++ ClassroomMadness/Assets/teacherBodyLocation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teacherBodyLocation : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject bodyProf;
+    public float haut;
+    public float z;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, cam.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+
+        transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y - haut, cam.transform.position.z) + cam.transform.forward * distance; ;
+    }
+}
