04a8cdfc0871ffe5147e928a244cb2425a3ba44b
Strapontin <antodu73@hotmail.fr>
2020-01-01 20:21:37 +0100

    Le prof peut frapper sur tous les élèves

--- ClassroomMadness/Assets/notreProg/Script/RegleScript.cs
+++ ClassroomMadness/Assets/notreProg/Script/RegleScript.cs
@@ -8,22 +8,27 @@ public class RegleScript : MonoBehaviour
     private GameObject rightHand;
     private GameObject leftHand;
 
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
         rightHand = GameObject.Find("LeftHand");
         leftHand = GameObject.Find("RightHand");
+
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigidbody.isKinematic = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Eleve") ||
+            collision.gameObject.CompareTag("EleveJoueur"))
         {
             rightHand.gameObject.GetComponent<Hand>().DetachObject(gameObject, false);
             leftHand.gameObject.GetComponent<Hand>().DetachObject(gameObject, false);
